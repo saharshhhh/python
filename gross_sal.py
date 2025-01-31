3371a7b6768c86162ee62e1c5f25a66ccc3c921c
saharshhhh <saharshpabbathi@gmail.com>
2025-01-31 09:32:11 +0530

    modified

--- gross_sal.py
+++ gross_sal.py
@@ -1,12 +1,12 @@
 bsal = int(input("Enter basic salary in Rs. "))
 if bsal < 10000:
-    hra = 0.67*bsal
-    da = 0.73*bsal
+    hra = 0.67 * bsal
+    da = 0.73 * bsal
 elif bsal < 20000:
-    hra = 0.69*bsal
-    da = 0.76*bsal
+    hra = 0.69 * bsal
+    da = 0.76 * bsal
 else:
-    hra = 0.73*bsal
-    da = 0.89*bsal
-gs = hra+da+bsal
-print("Gross salary is", gs, "/-")
+    hra = 0.73 * bsal
+    da = 0.89 * bsal
+gs = hra + da + bsal
+print("Gross salary is", gs, "/")
